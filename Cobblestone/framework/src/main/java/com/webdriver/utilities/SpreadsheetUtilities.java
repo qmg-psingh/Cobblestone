7cba4e9645b1b7c15005052dbdd4c111299d2808
Sheena Gurm <sgurm@bebotechnologies.com>
2019-04-09 20:03:44 +0530

    Added page objects and mapping utilities

--- Cobblestone/framework/src/main/java/com/webdriver/utilities/SpreadsheetUtilities.java
+++ Cobblestone/framework/src/main/java/com/webdriver/utilities/SpreadsheetUtilities.java
@@ -1,5 +1,6 @@
 package com.webdriver.utilities;
 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -35,28 +36,160 @@ public class SpreadsheetUtilities {
 
 
 	public ArrayList<ArrayList<String>> fetchCompleteDataOfSheet(String sheetName) {
-		return null;
+		try {
+			workbook = WorkbookFactory.create(new File(Utilities.getTestDataFilePath(workbookName)));
+		} catch (EncryptedDocumentException | IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		sheet = workbook.getSheet(sheetName);
+		ArrayList<ArrayList<String>> sheetData = new ArrayList<ArrayList<String>>();
+		DataFormatter dataFormatter = new DataFormatter();
+
+		for (Row row : sheet) {
+			ArrayList<String> rowData = new ArrayList<String>();
+			for (Cell cell : row) {
+				rowData.add(dataFormatter.formatCellValue(cell).trim());
+			}
+			sheetData.add(rowData);
+		}
 		
-		
+		try {
+			workbook.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sheetData;
 	}
 	
 	public Sheet addNewSheetInSpreadsheet(String sheetName) {
+		if (workbook != null) {
+			try {
+				workbook.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		FileInputStream fileinp;
+		FileOutputStream fileOut;
+		Sheet createdSheet=null;
 		
+		try {
+			fileinp = new FileInputStream(Utilities.getTestDataFilePath(workbookName));
+			workbook = new HSSFWorkbook(fileinp);
+			fileOut = new FileOutputStream(Utilities.getTestDataFilePath(workbookName));
+			
+			int index=workbook.getSheetIndex(sheetName);
+			
+			if(index!=-1){
+			workbook.removeSheetAt(index);
+			}
+			
+			createdSheet= workbook.createSheet(sheetName);
+			
+			
+			   workbook.write(fileOut);
+			    fileOut.close();
+			    fileinp.close();
+			    workbook.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		return null;
+		return createdSheet;
 	}
 	
 	public void addDataInSheet(String sheetName, LinkedHashSet<String> rowData, int rowIndex){
-	
+		try
+		   {
+		       FileInputStream myxls = new FileInputStream(Utilities.getTestDataFilePath(workbookName));
+		       HSSFWorkbook workbook = new HSSFWorkbook(myxls);
+		       HSSFSheet worksheet = workbook.getSheet(sheetName);
+		       Row row = worksheet.createRow(rowIndex);
+		       int i=0;
+		       for(String data : rowData){
+		    	   row.createCell(i).setCellValue(data);
+		    	   i++;
+		       }
+		       
+		       myxls.close();
+		       FileOutputStream output_file =new FileOutputStream(new File(Utilities.getTestDataFilePath(workbookName)));  
+		       //write changes
+		       workbook.write(output_file);
+		       output_file.close();
+		       workbook.close();
+		    }
+		    catch(Exception e)
+		    {
+		    }
 	}
 	
 	public void addDataInSheet(String sheetName, String columnHeader, String data, int rowIndex) {
-		
+		try {
+			FileInputStream myxls = new FileInputStream(Utilities.getTestDataFilePath(workbookName));
+			HSSFWorkbook workbook = new HSSFWorkbook(myxls);
+			HSSFSheet worksheet = workbook.getSheet(sheetName);
+			Row row = null;
+			
+			if (worksheet.getRow(rowIndex) == null) {
+				worksheet.createRow(rowIndex);
+			}
+
+			row = worksheet.getRow(rowIndex);
+
+			int columnIndex = 0;
+			Row headerRow = worksheet.getRow(0);
+			Iterator<Cell> cellIterator = headerRow.cellIterator();
+			while (cellIterator.hasNext()) {
+				Cell columnName = cellIterator.next();
+				if (columnHeader.equals(columnName.getRichStringCellValue().toString())) {
+					columnIndex = columnName.getColumnIndex();
+					break;
+				}
+			}
+
+			row.createCell(columnIndex).setCellValue(data);
+
+			myxls.close();
+			FileOutputStream output_file = new FileOutputStream(new File(Utilities.getTestDataFilePath(workbookName)));
+			// write changes
+			workbook.write(output_file);
+			output_file.close();
+			workbook.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String getDataFromSheet(String sheetName, String columnHeader, int rowIndex) {
-		return columnHeader;
-		
-		
+		String cellValue=null;
+		try {
+			FileInputStream myxls = new FileInputStream(Utilities.getTestDataFilePath(workbookName));
+			HSSFWorkbook workbook = new HSSFWorkbook(myxls);
+			HSSFSheet worksheet = workbook.getSheet(sheetName);
+			Row row = worksheet.getRow(rowIndex);
+
+			int columnIndex = 0;
+			Row headerRow = worksheet.getRow(0);
+			Iterator<Cell> cellIterator = headerRow.cellIterator();
+			while (cellIterator.hasNext()) {
+				Cell columnName = cellIterator.next();
+				if (columnHeader.equals(columnName.getRichStringCellValue().toString())) {
+					columnIndex = columnName.getColumnIndex();
+					break;
+				}
+			}
+
+			cellValue = row.getCell(columnIndex).getRichStringCellValue().getString();
+
+			myxls.close();
+			workbook.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return cellValue;
 	}
 }
--- Cobblestone/framework/src/main/java/com/webdriver/utilities/TestdataFileUtils.java
+++ Cobblestone/framework/src/main/java/com/webdriver/utilities/TestdataFileUtils.java
@@ -18,30 +18,44 @@ public class TestdataFileUtils {
 	
 
 	public TestdataFileUtils(String aggregatorName, String sheetName) {
-	
+		this.sheetName = sheetName;
+		this.aggregatorName = aggregatorName;
+		spreadsheetUtilities = new SpreadsheetUtilities("TestData_"+aggregatorName+".xls");
+		spreadsheetData = spreadsheetUtilities.fetchCompleteDataOfSheet(sheetName);
 	}
 
+	
 	public Iterator<Object[]> getTestDataForScript(){
+		ArrayList<Object[]> testData = new ArrayList<>();
 		
+		for (ArrayList<String> row : spreadsheetData) {
+			Object[] testDataRow = row.toArray();
+			testData.add(testDataRow);
+		}
 		
-		return null;
+		header = testData.get(0);
+		testData.remove(0);
+		
+		return testData.iterator();
 	}
 	
 	public Object[] getHeadersList(){
-		return null;
+		return header;
 	}
 	
 	public void createExpectedResultsSpreadsheet(){
-		
+		expectedDataSheetName=sheetName+"_E";
+		spreadsheetUtilities.addNewSheetInSpreadsheet(expectedDataSheetName);
 		
 	}
 	
 	public void addExpectedHeadersInNewSpreadsheet(LinkedHashSet expectedHeadersList){
-		
+		this.expectedHeadersList = expectedHeadersList;
+		spreadsheetUtilities.addDataInSheet(expectedDataSheetName, this.expectedHeadersList, 0);
 	}
 	
 	public void addExpectedResult(String  expectedHeader, String expectedAnswer, int rowIndex){
-		
+		spreadsheetUtilities.addDataInSheet(expectedDataSheetName, expectedHeader, expectedAnswer, rowIndex);
 	}
 	
 
